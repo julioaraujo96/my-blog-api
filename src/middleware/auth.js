9fd6b9580d31e41063bceb67b21066032a9d0f74
julioaraujo96 <49203294+julioaraujo96@users.noreply.github.com>
2022-04-19 12:13:16 +0100

    set auth middleware and user profile route

---
+++ src/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user');
+
+const auth = async (req,res,next) =>{
+    
+    try 
+    {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, 'YmxvZ2d5YXBw');
+        const user = await User.findOne({_id:decoded._id, 'tokens.token':token})
+
+        if(!user){
+            throw new Error();
+        }
+        //dar acesso ao handler da rota do user para que não seja necessário ir buscar à base de dados novamente.
+        req.user = user
+        next()
+    } 
+    catch (e) 
+    {
+        res.status(401).send({error:'Please Authenticate'})    
+    }
+}
+
+module.exports = auth
